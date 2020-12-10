5dbf6004d5bf3db21d4832ea9aaf2661dd8228b5
DmitrievLeo <leonidkdmitriev@gmail.com>
2020-12-10 17:29:28 +0300

    4

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div id="app">
-   
+    <TodoList v-bind:todos="todos" v-on:delete-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-
+import TodoList from './components/TodoList.vue'
 
 export default {
-  name: 'App',
-  
+  name: 'app',
+  data(){
+    return {
+      todos: ['Зарядка', 'Завтрак','Уборка','Прогулка']
+    }
+  },
+  components: {
+    TodoList
+  },
+  methods: {
+    deleteTodo(index){
+      this.todos.splice(index, 1);
+    }
+  }
 }
 </script>
 
